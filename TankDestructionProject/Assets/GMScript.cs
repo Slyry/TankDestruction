884205c81247637d87ae887e36b501abc6d1cd86
lex116 <lexus.lopez@loop.colum.edu>
2017-02-16 12:22:08 -0600

    tank stuff Lex
    my stuff

---
+++ TankDestructionProject/Assets/GMScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class GMScript : MonoBehaviour{
+
+    [SerializeField]
+    Text clock;
+
+    [SerializeField]
+    float RoundLimit;
+
+    [SerializeField]
+    Text P1HP;
+
+    [SerializeField]
+    float PlayerOneHealth;
+
+    [SerializeField]
+    Text Score;
+
+    [SerializeField]
+    float BuildingsToDestroy;
+
+    public float destroyedBuildings;
+
+    [SerializeField]
+    Text Results;
+
+    bool P1Victory;
+
+    bool P2Victory;
+
+    void Start ()
+    {
+        InvokeRepeating("GameClock", 0, 1);
+        P1Victory = false;
+        P2Victory = false;
+	}
+	
+	void Update ()
+    {
+        UpdatePlayerHealth();
+        UpdateBuildingDestructionProgress();
+        UpdateResults();
+	}
+
+    void UpdateResults()
+    {
+        if (P1Victory)
+        {
+            Results.text = "PLAYER 1 WINS!";
+        }
+
+        if (P2Victory)
+        {
+            Results.text = "PLAYER 2 WINS!";
+        }
+    }
+
+    void UpdateBuildingDestructionProgress()
+    {
+        Score.text = "Buildings Razed: " + destroyedBuildings;
+
+        if (destroyedBuildings >= BuildingsToDestroy)
+        {
+            P1Victory = true;
+        }
+    }
+
+    void UpdatePlayerHealth()
+    {
+        P1HP.text = "Armor: " + PlayerOneHealth;
+
+        if (PlayerOneHealth <= 0)
+        {
+            P1HP.text = "DESTROYED";
+            P2Victory = true;
+        }
+    }
+
+    void GameClock()
+    {
+        if (!P1Victory && !P2Victory)
+        {
+            RoundLimit--;
+            clock.text = "Time Remaining: " + RoundLimit;
+
+            if (RoundLimit <= 0)
+            {
+                clock.text = "TIME'S UP!";
+            }
+        }
+        else
+        {
+            clock.text = "Time Remaining: " + RoundLimit;
+        }
+    }
+}
---
+++ TankDestructionProject/Assets/Scripts/BuildingBehavior.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System;
+
+public class BuildingBehavior : MonoBehaviour
+
+{
+    [SerializeField]
+    GMScript gmScript;
+
+    [SerializeField]
+    float buildingHealth = 5;
+
+    [SerializeField]
+    Material normalMaterial;
+
+    [SerializeField]
+    Material redMaterial;
+
+    [SerializeField]
+    Material blackMaterial;
+
+    [SerializeField]
+    ParticleSystem explosion;
+
+    Renderer rend;
+
+    Collider bCollider;
+
+    void Awake ()
+    {
+        rend = this.GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Specular");
+        bCollider = this.GetComponent<BoxCollider>();
+    }
+
+    IEnumerator BuildingDamageLogic()
+    {
+        if (buildingHealth > 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                BuildingDamageFlashRed();
+                yield return new WaitForSeconds(.1f);
+                BuildingDamageFlashWhite();
+                yield return new WaitForSeconds(.1f);
+                Debug.Log("test" + i);
+            }
+        }
+
+        if (buildingHealth == 0)
+        {
+            
+            StartCoroutine(BuildingDamageDestroy());
+            explosion.Play();
+        }
+    }
+
+    IEnumerator BuildingDamageDestroy()
+    {
+        rend.material = blackMaterial;
+        bCollider.isTrigger = true;
+        yield return new WaitForSeconds(2f);
+        gmScript.destroyedBuildings = (gmScript.destroyedBuildings + 1);
+        Destroy(gameObject);
+    }
+
+    void BuildingDamageFlashRed()
+    {
+        
+        rend.material = redMaterial;
+    }
+
+    void BuildingDamageFlashWhite()
+    {
+
+        rend.material = normalMaterial;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.gameObject.tag == "PlayerOneBullet")
+        {
+            Destroy(col.gameObject);
+            buildingHealth--;
+            Debug.Log("Boop");
+            StartCoroutine(BuildingDamageLogic());
+        }
+    }
+}
